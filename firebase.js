0a796bd17e038c58eb9650f038334747065edc04
danycambrano <danycambrano1@gmail.com>
2020-11-19 17:10:43 -0600

    Archivos firebase

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBW1FHPtBZxUd9kKChQatV_sYjB65-0oUQ",
+    authDomain: "apppruebar.firebaseapp.com",
+    databaseURL: "https://apppruebar.firebaseio.com",
+    projectId: "apppruebar",
+    storageBucket: "apppruebar.appspot.com",
+    messagingSenderId: "906429860225",
+    appId: "1:906429860225:web:851a9ff146ee5aeccd8080"
+  };
+  // Initialize Firebase
+  const fb = firebase.initializeApp(firebaseConfig);
+
+  export const fb =fb.firestore();
